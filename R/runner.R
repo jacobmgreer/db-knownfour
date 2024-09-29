3a21da40cccbb11d7c0723f2e79232fc5f59a4fd
Jacob Greer <jacobmgreer@users.noreply.github.com>
2024-09-29 13:28:54 -0700

    overhaul

--- R/runner.R
+++ R/runner.R
@@ -155,10 +155,13 @@ for(j in unique(imdb_names$batch)) {
     filter(nconst %in% batch$nconst) %>%
     rename(tconst = knownForTitles) %>%
     left_join(imdb_basics, by = "tconst") %>%
+    arrange(iYear, nconst) %>%
     group_by(nconst) %>%
     summarize(
       content = n_distinct(tconst),
       contentQID = n_distinct(QID, na.rm = T),
+      iYears = paste(iYear, collapse = ","),
+      tconsts = paste(tconst, collapse = ","),
       minReleaseYear = min(iYear, na.rm = T),
       maxReleaseYear = max(iYear, na.rm = T),
       anyAdult = n_distinct(tconst[isAdult == 1], na.rm = T),
@@ -184,61 +187,88 @@ message(format(Sys.time(), '%H:%M:%S'), " - ", "Created ALL KF4 Name Sets")
 review_KF4 <-
   list.files("parquet/KF4", recursive = TRUE, full.names = TRUE) %>%
   map_dfr(., read_parquet) %>%
-  left_join(
-    wd_imdb_people,
-    by = "nconst"
-  )
+  left_join(wd_imdb_people, by = "nconst")
+
+review_KF4_decades <-
+  review_KF4 %>%
+  separate_longer_delim(iYears, delim = ",") %>%
+  reframe(
+    nconst,
+    iYears = 
+      case_when(
+        iYears == "NA" ~ NA,
+        .default = floor(as.numeric(iYears)/10)*10)) %>%
+  group_by(nconst, decade = iYears) %>%
+  summarize(n = n()) %>%
+  arrange(decade) %>%
+  pivot_wider(names_from = decade, names_prefix = "decade_", values_from = n)
 
 review_KF3 <-
   list.files("parquet/KF3", recursive = TRUE, full.names = TRUE) %>%
   map_dfr(., read_parquet) %>%
-  reframe(nconst, knownForProfession) %>%
-  left_join(review_KF4, by = "nconst")
+  left_join(review_KF4 %>% reframe(nconst, content), by = "nconst") %>%
+  filter(!is.na(content))
+
+review_KF3_decades_sum <-
+  review_KF3 %>%
+  left_join(review_KF4_decades, by = "nconst") %>%
+  select(-nconst, content) %>%
+  group_by(knownForProfession) %>% 
+  summarise(across(everything(), ~ifelse(all(is.na(.)), NA, sum(., na.rm = T))))
+
+review_KF3_decades_content <-
+  review_KF3 %>%
+  left_join(review_KF4_decades, by = "nconst") %>%
+  select(-nconst) %>%
+  group_by(knownForProfession, content) %>% 
+  summarise(across(everything(), ~ifelse(all(is.na(.)), NA, sum(., na.rm = T))))
 
 knownFour_summary <-
   review_KF3 %>%
-  filter(!is.na(content)) %>%
+  distinct(nconst, knownForProfession) %>%
+  left_join(review_KF4, by = "nconst") %>%
   group_by(knownForProfession) %>%
   summarize(
     people = n_distinct(nconst),
     credits = sum(content, na.rm = T),
     in_wikidata = n_distinct(QID, na.rm = T),
-    minReleaseYear = min(minReleaseYear, na.rm = T),
-    maxReleaseYear = max(maxReleaseYear, na.rm = T),
+    minKnownForYear = min(minReleaseYear, na.rm = T),
+    maxKnownForYear = max(maxReleaseYear, na.rm = T),
     anyAdult = sum(anyAdult, na.rm = T),
     anyTV = sum(anyTV, na.rm = T),
     anyGame = sum(anyGame, na.rm = T),
     anyShort = sum(anyShort, na.rm = T),
     anyFilm = sum(anyFilm, na.rm = T),
-    anyVideo = sum(anyVideo, na.rm = T)
-  )
+    anyVideo = sum(anyVideo, na.rm = T)) %>%
+  left_join(review_KF3_decades_sum, by = "knownForProfession") %>%
+  mutate(content = "Summary")
 
 knownFour_k4s <-
   review_KF3 %>%
-  filter(!is.na(content)) %>%
+  distinct(nconst, knownForProfession) %>%
+  left_join(review_KF4, by = "nconst") %>%
   group_by(knownForProfession, content) %>%
   summarize(
     people = n_distinct(nconst),
     credits = sum(content, na.rm = T),
     in_wikidata = n_distinct(QID, na.rm = T),
-    minReleaseYear = min(minReleaseYear, na.rm = T),
-    maxReleaseYear = max(maxReleaseYear, na.rm = T),
+    minKnownForYear = min(minReleaseYear, na.rm = T),
+    maxKnownForYear = max(maxReleaseYear, na.rm = T),
     anyAdult = sum(anyAdult, na.rm = T),
     anyTV = sum(anyTV, na.rm = T),
     anyGame = sum(anyGame, na.rm = T),
     anyShort = sum(anyShort, na.rm = T),
     anyFilm = sum(anyFilm, na.rm = T),
-    anyVideo = sum(anyVideo, na.rm = T)
-  )
+    anyVideo = sum(anyVideo, na.rm = T)) %>%
+  left_join(review_KF3_decades_content, by = c("knownForProfession", "content")) %>%
+  mutate(content = as.character(content))
 
 knownFours <-
-  bind_rows(
-    knownFour_k4s %>%
-      mutate(content = as.character(content)),
-    knownFour_summary %>%
-      mutate(content = "Summary")
-  ) %>%
-  arrange(knownForProfession, content)
+  bind_rows(knownFour_k4s, knownFour_summary) %>%
+  arrange(knownForProfession, content) %>%
+  mutate(
+    minKnownForYear = ifelse(is.infinite(minKnownForYear), NA, minKnownForYear),
+    maxKnownForYear = ifelse(is.infinite(maxKnownForYear), NA, maxKnownForYear))
 
 write_csv(knownFours, "knownfours.csv")
 
