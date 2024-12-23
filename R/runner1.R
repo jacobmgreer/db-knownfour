a93be0f18c5b4a860a23cf4682b7cfe4520c7121
Jacob Greer <jacobmgreer@users.noreply.github.com>
2024-12-23 01:22:28 -0800

    updates

--- R/runner1.R
+++ R/runner1.R
@@ -3,10 +3,9 @@ suppressMessages(library(arrow))
 options(readr.show_col_types = FALSE)
 options(warn=-1)
 
-dir.create("parquet", showWarnings = F)
-dir.create("parquet/step1", showWarnings = F)
-dir.create("parquet/step2", showWarnings = F)
-dir.create("parquet/step3", showWarnings = F)
+dir.create("KF4", showWarnings = F)
+dir.create("KF4/titles", showWarnings = F)
+dir.create("KF4/people", showWarnings = F)
 
 ## Load wd-imdb
 
@@ -14,6 +13,13 @@ wd_imdb_film <-
   readRDS(url("https://jacobmgreer.github.io/Nitrate-Datasets/datasets/film/wd_imdb_films.rds")) %>%
   group_by(tconst = imdb) %>%
   summarize(QID = paste(unique(QID), collapse = " || "))
+
+wd_imdb_people <-
+  readRDS(url("https://jacobmgreer.github.io/Nitrate-Datasets/datasets/people/wd_imdb_people.rds")) %>%
+  group_by(nconst = imdb) %>%
+  summarize(QID = paste(unique(QID), collapse = " || "))
+
+message(format(Sys.time(), '%H:%M:%S'), " - ", "Loaded Wikidata Queries")
 
 ## Load Name Basics
 
@@ -23,7 +29,8 @@ imdb_names <-
     delim = "\t", 
     escape_double = FALSE, 
     trim_ws = TRUE) %>%
-  reframe(nconst, primaryName, primaryProfession, knownForTitles)
+  reframe(nconst, primaryName, primaryProfession, knownForTitles) %>%
+  left_join(wd_imdb_people, by = "nconst")
 
 message(format(Sys.time(), '%H:%M:%S'), " - ", "Loaded IMDb Names")
 
@@ -39,35 +46,40 @@ imdb_basics <-
     tconst, 
     iType = titleType, 
     iYear = as.numeric(ifelse(startYear == "\\N", NA, startYear)), 
-    isAdult)
+    isAdult) %>%
+  left_join(wd_imdb_film, by = "tconst")
 
 message(format(Sys.time(), '%H:%M:%S'), " - ", "Loaded IMDb Content")
 
+rm(wd_imdb_film, wd_imdb_people)
+
 ### Batch Out Unknown Profession
 
-unknown_profession <-
+write_parquet(
   imdb_names %>%
-  filter(primaryProfession == "\\N") %>%
-  reframe(nconst)
-
-write_parquet(unknown_profession, "parquet/step1/unknown_profession.parquet")
+    filter(primaryProfession == "\\N") %>%
+    reframe(nconst, QID), 
+  "KF4/unknown_profession.parquet")
 
 ## Create KnownFor3 Profession Summary
 
-KF3_professions <-
+write_parquet(
   imdb_names %>%
-  reframe(nconst, knownForProfession = primaryProfession) %>%
-  separate_longer_delim(knownForProfession, delim = ",")
-
-write_parquet(KF3_professions, "parquet/step1/KF3.parquet")
+    reframe(nconst, QID, knownForProfession = primaryProfession) %>%
+    separate_longer_delim(knownForProfession, delim = ","), 
+  "KF4/KF3.parquet")
 
 ## KF3 Profession Summary
 
-profession_count <-
+write_parquet(
   KF3_professions %>%
-  count(knownForProfession)
-
-write_csv(profession_count, "parquet/step1/profession_count.csv")
+    group_by(knownForProfession) %>%
+    summarize(
+      n = n(),
+      nQID = n_distinct(nconst[!is.na(QID)]),
+      per_nQID = round(nQID / n * 100, digits = 2)
+    ), 
+  "KF4/profession_count.parquet")
 
 message(format(Sys.time(), '%H:%M:%S'), " - ", "Created KF3 Professions")
 
@@ -78,43 +90,80 @@ KF4_content <-
   filter(knownForTitles != "\\N") %>%
   reframe(nconst, tconst = knownForTitles) %>%
   separate_longer_delim(tconst, delim = ",") %>%
-  left_join(wd_imdb_film, by = "tconst") %>%
   left_join(imdb_basics, by = "tconst")
 
-KF4_TV <-
-  KF4_content %>% filter(iType %in% c("tvEpisode", "tvSeries", "tvShort", "tvMiniSeries", "tvSpecial", "tvMovie", "tvPilot"))
-write_parquet(KF4_TV, "parquet/step1/KF4_TV.parquet")
-
-KF4_VideoGames <-
-  KF4_content %>% filter(iType == "videoGame")
-write_parquet(KF4_TV, "parquet/step1/KF4_VideoGames.parquet")
-
-KF4_Shorts <-
-  KF4_content %>% filter(iType == "short")
-write_parquet(KF4_TV, "parquet/step1/KF4_Shorts.parquet")
-
-KF4_Videos <-
-  KF4_content %>% filter(iType == "video")
-write_parquet(KF4_TV, "parquet/step1/KF4_Videos.parquet")
-
-KF4_Movies <-
-  KF4_content %>% filter(iType == "movie")
-write_parquet(KF4_TV, "parquet/step1/KF4_Movies.parquet")
-
-KF4_isAdult <-
-  KF4_content %>% filter(isAdult == 1)
-write_parquet(KF4_TV, "parquet/step1/KF4_isAdult.parquet")
-
+message(format(Sys.time(), '%H:%M:%S'), " - ", "Created KF4 Content")
 
 ## Batch Out Non-KF4 Content
 
-unknown_for <-
-  imdb_basics %>%
-  filter(!tconst %in% KF4_content$knownForTitles) %>%
-  left_join(wd_imdb_film, by = "tconst")
-
 write_parquet(
-  unknown_for %>% reframe(tconst, iType, iYear, isAdult, QID), 
-  "parquet/step1/unknown_for.parquet")
+  imdb_basics %>%
+    filter(!tconst %in% KF4_content$knownForTitles) %>% 
+    reframe(tconst, iType, iYear, isAdult, QID), 
+  "KF4/unknown_for.parquet")
 
 message(format(Sys.time(), '%H:%M:%S'), " - ", "Created Unknown Content")
+
+rm(imdb_basics, imdb_names)
+
+KF4_TV <-
+  KF4_content %>% filter(iType %in% c("tvEpisode", "tvSeries", "tvShort", "tvMiniSeries", "tvSpecial", "tvMovie", "tvPilot"))
+write_parquet(
+  KF4_content %>% 
+    filter(iType %in% 
+      c(
+        "tvEpisode", 
+        "tvSeries", 
+        "tvShort", 
+        "tvMiniSeries", 
+        "tvSpecial", 
+        "tvMovie", 
+        "tvPilot"
+      )
+    ), 
+  "KF4/titles/KF4_TV.parquet")
+
+write_parquet(
+  KF4_content %>% 
+    filter(iType == "videoGame"), 
+  "KF4/titles/KF4_VideoGames.parquet")
+
+write_parquet(
+  KF4_content %>% 
+    filter(iType == "short"), 
+  "KF4/titles/KF4_Shorts.parquet")
+
+write_parquet(
+  KF4_content %>% 
+    filter(iType == "video"), 
+  "KF4/titles/KF4_Videos.parquet")
+
+write_parquet(
+  KF4_content %>% 
+    filter(iType == "movie"), 
+  "KF4/titles/KF4_Movies.parquet")
+
+write_parquet(
+  KF4_content %>% 
+    filter(isAdult == 1), 
+  "KF4/titles/KF4_isAdult.parquet")
+
+message(format(Sys.time(), '%H:%M:%S'), " - ", "Batched KF4 Title Sets")
+
+rm(list=ls())
+
+for (i in list.files("basics/KF4", pattern = "*.parquet", full.names = T)) {
+  write_parquet(
+    read_parquet(i) %>%
+      arrange(iYear, nconst) %>%
+      group_by(nconst) %>%
+      summarize(
+        content = n_distinct(tconst),
+        contentQID = n_distinct(QID, na.rm = T),
+        minReleaseYear = min(iYear, na.rm = T),
+        maxReleaseYear = max(iYear, na.rm = T)
+      ), 
+    paste0("KF4/people/", basename(i)))
+}
+
+message(format(Sys.time(), '%H:%M:%S'), " - ", "Created KF4 People Sets")
--- R/runner2.R
+++ R/runner2.R
@@ -3,19 +3,25 @@ suppressMessages(library(arrow))
 options(readr.show_col_types = FALSE)
 options(warn=-1)
 
+dir.create("KF4", showWarnings = F)
+
 # Batch Out KF4 Content by Name Sets
 
-KF4_batch <-
-  read_parquet("/Users/jakeob/Documents/GitHub/compile/db-knownfour/parquet/step1/KF4_isAdult.parquet") %>%
-  arrange(iYear, nconst) %>%
-  group_by(nconst) %>%
-  summarize(
-    content = n_distinct(tconst),
-    contentQID = n_distinct(QID, na.rm = T),
-    iYears = paste(iYear, collapse = ","),
-    minReleaseYear = min(iYear, na.rm = T),
-    maxReleaseYear = max(iYear, na.rm = T)
-  )
+KF4_types <- list.files("basics/KF4", pattern = "*.parquet", full.names = T)
+
+for (i in KF4_types) {
+  batch <-
+    read_parquet(i) %>%
+    arrange(iYear, nconst) %>%
+      group_by(nconst) %>%
+      summarize(
+        content = n_distinct(tconst),
+        contentQID = n_distinct(QID, na.rm = T),
+        minReleaseYear = min(iYear, na.rm = T),
+        maxReleaseYear = max(iYear, na.rm = T)
+      )
+  write_parquet(batch, paste0("KF4/", basename(i)))
+}
 
 write_parquet(KF4_batch, "parquet/step2/KF4.parquet")
 
